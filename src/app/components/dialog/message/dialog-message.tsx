d3d8fdfe1c7e43b0eb14de289ba1f750cf29898d
zhouzekun <zhouzk3@chinatelecom.cn>
2024-07-30 18:07:01 +0800

    feat 添加session持久化存储

--- src/app/components/dialog/message/dialog-message.tsx
+++ src/app/components/dialog/message/dialog-message.tsx
@@ -1,9 +1,11 @@
+import {useLocation, useParams} from 'react-router-dom';
 import styles from "./dialog-message.module.scss";
-import {useLocation, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
-import {Message, MessageDirection, MessageType} from "@/app/types/chat";
-import {DialogMessageItem} from "@/app/components/dialog/message/dialog-message-item";
-import {DialogMessageInput} from "@/app/components/dialog/message/dialog-message-input";
+import {userChatStore} from "@/app/store/chat-store";
+import userScrollToBottom from '@/app/hooks/useScrollToBottom';
+import {Message, MessageDirection, MessageRole, MessageType} from "@/app/types/chat";
+import {DialogMessageItem} from "@/app/components/dialog/message/item/dialog-message-item";
+import {DialogMessageInput} from "@/app/components/dialog/message/input/dialog-message-input";
 
 interface Props {
     id: string,
@@ -16,41 +18,43 @@ interface Props {
  */
 export function DialogMessage() {
     const {id} = useParams();
+    const chatStore = userChatStore();
+    const currentSession = chatStore.currentSession();
+    const [messages, setMessages] = useState<Message[]>([]);
     const location = useLocation();
+    const {scrollRef, setAutoScroll, scrollToBottom} = userScrollToBottom();
     const title = location.state?.title || "新的对话";
-    const [messages, setMessages] = useState<Message[]>([])
 
-    // 可以通过接口查询数据
+    // 也可以通过接口查询数据
     const fetchDetail = async () => {
-
-        const message01: Message = {
-            avatar: "/role/psychological.png",
-            message: "吹灭别人的灯，不会照亮自己吹灭别人的灯，不会照亮自己吹灭别人的灯，不会照亮自己吹灭别人的灯，不会照亮自己吹灭别人的灯，不会照亮自己吹灭别人的灯，不会照亮自己吹灭别人的灯，不会照亮自己吹灭别人的灯，不会照亮自己",
-            message_type: MessageType.Text,
-            time: Date.now(),
-            direction: MessageDirection.Receive
-        }
-
-        const message02: Message = {
-            avatar: "/role/runny-nose.png",
-            message: "大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！大师我悟了！",
-            message_type: MessageType.Text,
-            time: Date.now(),
-            direction: MessageDirection.Send
-        }
-
-        setMessages([message01, message02]);
+        const session = await chatStore.currentSession();
+        const messages = session?.messages;
+        setMessages(messages);
     }
 
     // 输入事件
     const onEnter = (value: string) => {
-        setMessages([...messages, {
+        const newMessage01 = {
             avatar: "/role/runny-nose.png",
-            message: value,
+            content: value,
             message_type: MessageType.Text,
             time: Date.now(),
-            direction: MessageDirection.Send
-        }])
+            direction: MessageDirection.Send,
+            role: MessageRole.user
+        }
+
+        const newMessage02 = {
+            avatar: "/role/wali.png",
+            content: "`ChatGPT` 接口尚未对接，暂时还不能回复 **！！！**",
+            message_type: MessageType.Text,
+            time: Date.now(),
+            direction: MessageDirection.Receive,
+            role: MessageRole.system
+        }
+        setMessages([...messages, newMessage01, newMessage02]);
+
+        chatStore.onSendMessage(newMessage01);
+        chatStore.onSendMessage(newMessage02);
     }
 
     // 刷新数据
@@ -59,16 +63,48 @@ export function DialogMessage() {
         });
     }, [id]);
 
+    const clearContextIndex =
+        (currentSession.clearContextIndex ?? -1) >= 0
+            ? currentSession.clearContextIndex!
+            : -1;
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.header}>{title}</div>
-            <div className={styles.scroll}>
+            <div className={styles.scroll} ref={scrollRef}>
                 {messages?.map(
-                    (message, index) => <DialogMessageItem message={message} key={index}/>)
+                    (message, index) => {
+                        const shouldShowClearContextDivider = index === clearContextIndex - 1;
+                        return <>
+                            <DialogMessageItem message={message} key={index} parentRef={scrollRef}/>
+                            {shouldShowClearContextDivider && <ClearContextDivider/>}
+                        </>
+                    })
                 }
             </div>
             <DialogMessageInput onEnter={onEnter}/>
         </div>
     );
 
+}
+
+/**
+ * 清除上下文对话信息
+ * @constructor
+ */
+function ClearContextDivider() {
+    const chatStore = userChatStore();
+
+    return (
+        <div
+            className={styles["clear-context"]}
+            onClick={() =>
+                chatStore.updateCurrentSession(
+                    (session) => (session.clearContextIndex = undefined),
+                )
+            }
+        >
+            <div className={styles["clear-context-tips"]}>上下文已清除</div>
+        </div>
+    );
 }
